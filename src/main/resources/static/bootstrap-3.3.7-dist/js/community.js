678f3e36773a8d5587314267de0221df60aa9ac0
KlayHU <852827312@qq.com>
2020-03-13 18:23:34 +0800

    未登录评论后重新请求页面完成

--- src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
+++ src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
@@ -18,10 +18,15 @@ function Post() {
             "type":1
         }),
         success: function (response) {
-            if(response.code==200){
-                $("#comment_section").hide()
-            }else{
-                alert(response.message)
+            if (response.code == 200) {
+                $("#comment_section").hide();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                }if(isAccepted){
+                    window.open("https://github.com/login/oauth/authorize?client_id=Iv1.3129381aab788a9b&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
+                    window.localStorage.setItem("closable","true")
+                }
             }
             console.log(response)
         },
