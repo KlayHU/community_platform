b2203c33db698f9241defc50e68cd37ab26bd304
KlayHU <852827312@qq.com>
2020-03-18 21:26:29 +0800

    新增上传图片的功能

---
+++ src/main/java/com/klay/community/controller/FileController.java
@@ -0,0 +1,24 @@
+package com.klay.community.controller;
+
+import com.klay.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @description:
+ * @author: KlayHu
+ * @create: 2020/3/18 15:58
+ **/
+@Controller
+public class FileController {
+
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/bootstrap-3.3.7-dist/images/githubAvatar.png");
+        return fileDTO;
+    }
+}
---
+++ src/main/java/com/klay/community/dto/FileDTO.java
@@ -0,0 +1,15 @@
+package com.klay.community.dto;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: KlayHu
+ * @create: 2020/3/18 15:59
+ **/
+@Data
+public class FileDTO {
+    private int success;
+    private String message;
+    private String url;
+}
