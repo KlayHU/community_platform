80e8fcf5b50a60a77ca8479ce8dd7989496288ae
KlayHU <852827312@qq.com>
2020-03-29 19:41:35 +0800

    引入阿里云oss调试完成

--- src/main/java/com/klay/community/controller/FileController.java
+++ src/main/java/com/klay/community/controller/FileController.java
@@ -2,6 +2,7 @@ package com.klay.community.controller;
 
 import com.klay.community.dto.FileDTO;
 import com.klay.community.provider.AliyunProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @description:
@@ -17,22 +19,27 @@ import java.io.IOException;
  * @create: 2020/3/18 15:58
  **/
 @Controller
+@Slf4j
 public class FileController {
 
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload(HttpServletRequest request){
+    public FileDTO upload(HttpServletRequest request) {
         MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
         MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
         AliyunProvider aliyunProvider = new AliyunProvider();
         try {
-            aliyunProvider.upload(file.getInputStream(),file.getOriginalFilename());
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(1);
+            String uploadFile = aliyunProvider.upload(file.getInputStream(), file.getOriginalFilename());
+            fileDTO.setUrl(uploadFile);
+            return fileDTO;
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("upload error", e);
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(0);
+            fileDTO.setMessage("上传失败");
+            return new FileDTO();
         }
-        FileDTO fileDTO = new FileDTO();
-        fileDTO.setSuccess(1);
-        fileDTO.setUrl("/bootstrap-3.3.7-dist/images/githubAvatar.png");
-        return fileDTO;
     }
 }
--- src/main/java/com/klay/community/provider/AliyunProvider.java
+++ src/main/java/com/klay/community/provider/AliyunProvider.java
@@ -2,39 +2,58 @@ package com.klay.community.provider;
 
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.model.PutObjectRequest;
-import org.apache.commons.lang3.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
-import java.util.UUID;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 /**
  * @description:
  * @author: KlayHu
  * @create: 2020/3/27 13:33
  **/
+@Slf4j
 public class AliyunProvider {
     public String upload(InputStream inputStream, String fileName) {
-        String generatedFileName;
-        String[] filePaths = fileName.split("\\.");
-        if (filePaths.length > 1) {
-            generatedFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length - 1];
-        } else {
-            return null;
-        }
-        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+
+        // 生成上传文件名
+        String finalFileName = System.currentTimeMillis() + "" + new SecureRandom().nextInt(0x0400) + suffixName;
+        String objectName = sdf.format(new Date()) + "/" + finalFileName;
+//        String[] filePaths = fileName.split("\\.");
+//        if (filePaths.length > 1) {
+//            generatedFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length - 1];
+//        } else {
+//            return null;
+//        }
+
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+        //RAM子账号
         String accessKeyId = "LTAI4FvegE4eKucgXMMJVNmZ";
         String accessKeySecret = "ZFZEuTRsdEccH2vzEnehpXnn2xNgK0";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//        添加ContentType
+//        ObjectMetadata objectMetadata = new ObjectMetadata();
+//        objectMetadata.setContentDisposition("attachment;");
+        //objectMetadata.setContentType(fileName.substring(fileName.lastIndexOf(".")));
+        ossClient.putObject("community-platform", objectName, inputStream);
 
-        ossClient.putObject("community-platform", "community-platform/" + UUID.randomUUID().toString(),inputStream);
-        System.out.println("上传成功:"+inputStream.toString());
-        // 关闭OSSClient。
+        Date expiration = new Date(System.currentTimeMillis() + 3600 * 1000);
+
+        URL url = ossClient.generatePresignedUrl("community-platform", objectName, expiration);
+
+        log.info("图片已成功上传:" + inputStream.toString());
+        // 关闭OSSClient
         ossClient.shutdown();
-        return generatedFileName;
+        return url.toString();
     }
 }
