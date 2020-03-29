94c41a49cf535151b11edbccc41945a9a8fb7a36
KlayHU <852827312@qq.com>
2020-03-29 16:46:33 +0800

    文件流上传成功

--- src/main/java/com/klay/community/controller/FileController.java
+++ src/main/java/com/klay/community/controller/FileController.java
@@ -5,6 +5,11 @@ import com.klay.community.provider.AliyunProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * @description:
@@ -16,12 +21,18 @@ public class FileController {
 
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload(){
-//        FileDTO fileDTO = new FileDTO();
-//        fileDTO.setSuccess(1);
-//        fileDTO.setUrl("/bootstrap-3.3.7-dist/images/githubAvatar.png");
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
         AliyunProvider aliyunProvider = new AliyunProvider();
-        aliyunProvider.upload();
+        try {
+            aliyunProvider.upload(file.getInputStream(),file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/bootstrap-3.3.7-dist/images/githubAvatar.png");
         return fileDTO;
     }
 }
--- src/main/java/com/klay/community/provider/AliyunProvider.java
+++ src/main/java/com/klay/community/provider/AliyunProvider.java
@@ -3,9 +3,10 @@ package com.klay.community.provider;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.model.PutObjectRequest;
+import org.apache.commons.lang3.StringUtils;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
+import java.io.*;
+import java.util.UUID;
 
 /**
  * @description:
@@ -13,30 +14,27 @@ import java.io.File;
  * @create: 2020/3/27 13:33
  **/
 public class AliyunProvider {
-    public void upload() {
+    public String upload(InputStream inputStream, String fileName) {
+        String generatedFileName;
+        String[] filePaths = fileName.split("\\.");
+        if (filePaths.length > 1) {
+            generatedFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length - 1];
+        } else {
+            return null;
+        }
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
-// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
         String accessKeyId = "LTAI4FvegE4eKucgXMMJVNmZ";
         String accessKeySecret = "ZFZEuTRsdEccH2vzEnehpXnn2xNgK0";
 
-// 创建OSSClient实例。
+        // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
-// 创建PutObjectRequest对象。
-        PutObjectRequest putObjectRequest = new PutObjectRequest("community-platform", "community-platform/community", new File("D:\\image\\aliyun.jpg"));
-
-// 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
-// ObjectMetadata metadata = new ObjectMetadata();
-// metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
-// metadata.setObjectAcl(CannedAccessControlList.Private);
-// putObjectRequest.setMetadata(metadata);
-
-// 上传文件。
-        ossClient.putObject(putObjectRequest);
-        System.out.println("上传成功:"+ putObjectRequest);
-
-// 关闭OSSClient。
+        ossClient.putObject("community-platform", "community-platform/" + UUID.randomUUID().toString(),inputStream);
+        System.out.println("上传成功:"+inputStream.toString());
+        // 关闭OSSClient。
         ossClient.shutdown();
+        return generatedFileName;
     }
 }
